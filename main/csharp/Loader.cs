e393ae1b6aaa5a5df964ce3385f5fa9175e3ea7d
Davin-Herson <davinherson18@gmail.com>
2025-07-19 18:00:52 +0700

    Create Loader.cs

---
+++ main/csharp/Loader.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Minecraft {
+    public class Load {
+        public static void Load() {
+            for (int i = 0; i <= 10; i++) {
+                Console.WriteLine("[][][][][][][][][][]");
+            }
+        }
+    }
+}
