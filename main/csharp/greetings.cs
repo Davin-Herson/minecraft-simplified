a981c612a24ae318724dd01f73ab9af37718780c
Davin-Herson <davinherson18@gmail.com>
2025-07-19 22:18:47 +0700

    Create greetings.cs

---
+++ main/csharp/greetings.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class ExampleClass {
+    public static void Method(string name, int age) {
+        Console.WriteLine($"Hello {name}! Your age is {age}."); 
+    {
+}
+
+public class Program {
+    public static void Main(string[] args) {
+        // Method calls...
+        ExampleClass.Method("Davin", 30);
+    }
+}
