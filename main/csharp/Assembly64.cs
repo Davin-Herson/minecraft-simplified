32841ad54ca379c5b35f895329a74f9761c1665d
Davin-Herson <davinherson18@gmail.com>
2025-07-19 22:01:48 +0700

    Create Assembly64.cs

---
+++ main/csharp/Assembly64.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Assembly;
+
+namespace Setter {
+    public class CLI {
+        public static void Main(string[] args) {
+            Console.WriteLine("{ASM: 64}");
+        }
+    }
+}
